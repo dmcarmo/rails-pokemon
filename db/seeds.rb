ebc5d521ff5930a883efca9be2a94d27b5a2f1e9
David Carmo <dmcarmo@gmail.com>
2024-10-03 19:21:47 +0100

    All setup for working on the pokemon index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Pokemon.destroy_all
+
+puts 'Creating pokemons...'
+response = URI.open('https://pokeapi.co/api/v2/pokemon?limit=50').read
+results = JSON.parse(response)['results']
+results.each do |result|
+  info = JSON.parse(URI.open(result['url']).read)
+  pokemon = Pokemon.create(name: info['name'].capitalize, element_type: info['types'].first['type']['name'])
+  pokemon.photo.attach(io: URI.open(info['sprites']['front_default']), filename: "#{info['name']}.png", content_type: 'image/png')
+  puts "Screeahhhhh! #{pokemon.name} created!"
+end
+
+puts "Finished! Created #{Pokemon.count} pokemons."
